279309ed69d16652fd3133e498e23ca44ed73261
Teofilius <tvirbickas@gmail.com>
2021-08-29 16:36:36 +0300

    decided to render a seperate html for mobile info message

--- app.js
+++ app.js
@@ -29,6 +29,6 @@ app.get('/', function(request, response){
       // render the experiment script, along with some data (here, just the trial_id);
       response.render('index.html')
     } else {
-      response.send('<div class="jspsych-content">Please try again</div>');
+      response.render('mobile.html');
     }
 });
